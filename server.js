e8f53edfef7f171e786511257badc7d72c36441a
Byung-Jin Lee <byung2523@gmail.com>
2019-10-15 14:22:12 -0400

    Adding the Back-end and the Server

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'byung-jin',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => { res.send(db.user) })
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req,res) => { image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req,res) => { image.handleApiCall(req, res)})
+
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
+
+/*
+/--> res = this is working "Root Route"
+/signing --> POST = success/fail
+/register --> POST = user
+/profile/:userID --> GET = user
+/image --> PUT --> user
+*/ 
+
+// bcrypt.hash(password, null, null, function(err,hash) {
+// 		console.log(hash);
+// 	});
