333ab5ece82b26d9f3c6ef56d33611c540cdbfad
Byung-Jin Lee <byung2523@gmail.com>
2020-11-10 17:57:41 -0500

    Updated controllers/image.js

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,12 +1,12 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'b7c441837c3a48dea57dde19fe9ee168'
+ apiKey: 'Your API Key'
 });
 
 const handleApiCall = (req, res) => {
 	app.models
-	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
 	.then(data => {
 		res.json(data);
 	})
